4691eebe8b5eb637a1000d2046e551e22a5ea684
linknmike <mikeheidal@gmail.com>
2020-10-31 19:07:21 -0700

    added main function
    creates that one pattern of strings!

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,30 @@ import java.util.List;
 
 public class Main {
 
+        public static void main (String[] args){
+        String str = "1";
+        System.out.println("0: " + str);
+        for(int i = 0; i < 32; i++)
+        {
+            str = recurse(str, 0);
+            System.out.println((i + 1) + ": " + str);
+        }
+    }
+
+    private static String recurse(String str, int start) {
+        StringBuilder out = new StringBuilder();
+        char cur = str.charAt(start);
+        int i;
+        for (i = start; i < str.length(); i++) {
+            if (str.charAt(i) != cur) {
+                break;
+            }
+        }
+        out.append(i - start);
+        out.append(cur);
+        if (i < str.length()) {
+            out.append(recurse(str, i));
+        }
+        return out.toString();
+    }
 }
